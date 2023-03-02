552a9b1ab0382e917f36db99e3aa45acada36597
Sanjana-11 <sanjanakalathil@gmail.com>
2023-03-02 20:01:49 +0530

    made changes

--- Main.java
+++ Main.java
@@ -1,32 +1,41 @@
 package exercise05;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 class Browser {
 
-	static ArrayList<String> bookmarksList = new ArrayList<>();
+	ArrayList<String> bookmarksList = new ArrayList<>();
+	static List<String> urlList = new ArrayList<>();
 
-	public static ArrayList<String> getBookmarksList() {
-		return bookmarksList;
-	}
-
-	public static void setBookmarksList(ArrayList<String> bookmarksList) {
-		Browser.bookmarksList = bookmarksList;
+	public List<String> getUrlList() {
+		return urlList;
 	}
 
 	class Bookmarks {
 		void addBookmark(String url) {
 			bookmarksList.add(url);
 		}
+
+		void displayBookmark() {
+			System.out.println("Displaying bookmarked urls : ");
+			for (String url : bookmarksList) {
+				System.out.println(url);
+			}
+		}
 	}
 
 	static class History {
+		void addUrl(String url) {
+			urlList.add(url);
+		}
+
 		void displayHistory() {
-			System.out.println("Displaying history : ");
-			for (String url : bookmarksList) {
+			System.out.println("Displaying url history : ");
+			for (String url : urlList) {
 				System.out.println(url);
 			}
 		}
@@ -34,7 +43,7 @@ class Browser {
 
 }
 
-interface shortcutsInterface {
+interface ShortcutsInterface {
 	public void addShortcut();
 
 	public void displayShortcut();
@@ -48,32 +57,38 @@ public class Main {
 		Browser browser = new Browser();
 		Browser.Bookmarks bookmark = browser.new Bookmarks();
 
+		Browser.History history = new Browser.History();
+
 		do {
 			System.out.println("Enter url");
 			Scanner sc = new Scanner(System.in);
 			String url = sc.nextLine();
-
-			bookmark.addBookmark(url);
-
+			history.addUrl(url);
+			System.out.println("Bookmark url (Yes/No)");
+			String bookmarkUrl = sc.nextLine();
+			if (bookmarkUrl.equalsIgnoreCase("yes")) {
+				bookmark.addBookmark(url);
+			}
 			System.out.println("Do you wish to continue? (Yes/No)");
 			ch = sc.nextLine();
 		} while (ch.equalsIgnoreCase("yes"));
+		bookmark.displayBookmark();
 
-		HashMap<String, String> shortcutMap = new HashMap<>();
+		LinkedHashMap<String, String> shortcutsMap = new LinkedHashMap<>();
 
-		shortcutsInterface shortcuts = new shortcutsInterface() {
+		ShortcutsInterface shortcuts = new ShortcutsInterface() {
 			public void addShortcut() {
-				ArrayList<String> bookmarksList = Browser.getBookmarksList();
-				for (String obj : bookmarksList) {
-					System.out.println("Enter shortcut for " + obj);
+				List<String> urlList = browser.getUrlList();
+				for (String url : urlList) {
+					System.out.println("Enter shortcut for " + url);
 					Scanner sc1 = new Scanner(System.in);
 					String shortcut = sc1.nextLine();
-					shortcutMap.put(shortcut, obj);
+					shortcutsMap.put(shortcut, url);
 				}
 			}
 
 			public void displayShortcut() {
-				for (Map.Entry<String, String> entry : shortcutMap.entrySet()) {
+				for (Map.Entry<String, String> entry : shortcutsMap.entrySet()) {
 					System.out.println(entry.getKey() + ": " + entry.getValue());
 				}
 
@@ -83,7 +98,6 @@ public class Main {
 		shortcuts.addShortcut();
 		shortcuts.displayShortcut();
 
-		Browser.History history = new Browser.History();
 		history.displayHistory();
 	}
 }
